8efe5e7a7e8390933659058a68fef8d9ff269362
raneem-yad <eng.raneem.yad@gmail.com>
2024-05-03 12:38:57 +0200

    adding is_owner with permission

--- instructor/serializers.py
+++ instructor/serializers.py
@@ -4,8 +4,13 @@ from .models import Instructor
 
 class InstructorSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')  # one-to-one relationship
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
 
     class Meta:
         model = Instructor
         fields = ['id', 'owner', 'full_name', 'bio', 'job_title', 'website_link', 'linkedin_link',
-                  'image', 'created_date', 'updated_date']
+                  'image', 'is_owner', 'created_date', 'updated_date']
--- instructor/views.py
+++ instructor/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Instructor
 from .serializers import InstructorSerializer
+from wissen_api.permissions import IsOwnerOrReadOnly
 
 
 class InstructorList(APIView):
@@ -16,23 +17,26 @@ class InstructorList(APIView):
 
 class InstructorDetails(APIView):
     serializer_class = InstructorSerializer
+    permission_classes = [IsOwnerOrReadOnly]
 
     def get_object(self, pk):
         try:
             instructor = Instructor.objects.get(pk=pk)
+            self.check_object_permissions(self.request, instructor)
             return instructor
         except Instructor.DoesNotExist:
             raise Http404
 
     def get(self, request, pk):
         instructor = self.get_object(pk)
+
         serializer = InstructorSerializer(instructor, context={'request': request})
         return Response(serializer.data)
 
     @swagger_auto_schema(request_body=InstructorSerializer)
     def put(self, request, pk):
         instructor = self.get_object(pk)
-        serializer = InstructorSerializer(instructor, data=request.data)
+        serializer = InstructorSerializer(instructor, data=request.data,  context={'request': request})
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
