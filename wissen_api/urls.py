3eec5ef32905502f7ab323831e826ae5e71fa763
raneem-yad <eng.raneem.yad@gmail.com>
2024-05-08 15:45:51 +0200

    delete the swagger link

--- wissen_api/urls.py
+++ wissen_api/urls.py
@@ -34,7 +34,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path("api-auth/", include('rest_framework.urls')),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path(
