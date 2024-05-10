0f432d90f73962cca353f572c67f2d34e61317ae
raneem-yad <eng.raneem.yad@gmail.com>
2024-05-10 09:11:18 +0200

    dj-rest-auth Bug Fix

--- wissen_api/urls.py
+++ wissen_api/urls.py
@@ -21,6 +21,8 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+from wissen_api.views import logout_route
+
 schema_view = get_schema_view(
     openapi.Info(
         title="Wissen Website API",
@@ -37,6 +39,8 @@ urlpatterns = [
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path("api-auth/", include('rest_framework.urls')),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    # our logout route has to be above the default one to be matched first
+    path('dj-rest-auth/logout/', logout_route),
     path(
         'dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
     ),
---
+++ wissen_api/views.py
@@ -0,0 +1,38 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .settings import (
+    JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE, JWT_AUTH_SAMESITE,
+    JWT_AUTH_SECURE,
+)
+
+
+# dj-rest-auth logout view fix
+@api_view(['POST'])
+def logout_route(request):
+    """
+    Addresses an issue with:
+    dj-rest-auth has a bug that doesn't allow users to log out.
+    we set both cookies to an empty string and pass additional
+    attributes like secure, httponly and samesite, which was
+    left out by mistake by the library.
+    """
+    response = Response()
+    response.set_cookie(
+        key=JWT_AUTH_COOKIE,
+        value='',
+        httponly=True,
+        expires='Thu, 01 Jan 1970 00:00:00 GMT',
+        max_age=0,
+        samesite=JWT_AUTH_SAMESITE,
+        secure=JWT_AUTH_SECURE,
+    )
+    response.set_cookie(
+        key=JWT_AUTH_REFRESH_COOKIE,
+        value='',
+        httponly=True,
+        expires='Thu, 01 Jan 1970 00:00:00 GMT',
+        max_age=0,
+        samesite=JWT_AUTH_SAMESITE,
+        secure=JWT_AUTH_SECURE,
+    )
+    return response
