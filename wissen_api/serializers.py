4a5569da3e6da7792231555dae36e7f294696e8f
raneem-yad <eng.raneem.yad@gmail.com>
2024-05-10 13:45:31 +0200

    custom registration form

--- wissen_api/serializers.py
+++ wissen_api/serializers.py
@@ -18,11 +18,13 @@ class CurrentUserSerializer(UserDetailsSerializer):
 
 class CustomRegisterSerializer(RegisterSerializer):
     is_instructor = serializers.BooleanField()
+    full_name = serializers.CharField()
 
     def custom_signup(self, request, user):
         is_instructor = self.validated_data.get('is_instructor', False)
+        full_name = self.validated_data.get('full_name', False)
 
         if is_instructor:
-            Instructor.objects.create(owner=user)
+            Instructor.objects.create(owner=user, full_name=full_name)
         else:
-            Learner.objects.create(owner=user)
+            Learner.objects.create(owner=user, full_name=full_name)
