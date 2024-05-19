90f993378100d0d8351901caef79e5208e6d000d
raneem-yad <eng.raneem.yad@gmail.com>
2024-05-19 10:00:43 +0200

    add new profile_id field and docstring

--- learner/models.py
+++ learner/models.py
@@ -1,14 +1,32 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
 from django_resized import ResizedImageField
 
 
-
 class Learner(models.Model):
-    owner = models.OneToOneField(User, on_delete=models.CASCADE )
+    """
+       A model representing a learner profile associated with a user.
+
+       Fields:
+       - owner: A one-to-one relationship with the User model.
+       - full_name: The full name of the learner.
+       - bio: A brief biography of the learner.
+       - profile_id: A unique identifier for the profile, automatically generated if not provided.
+       - image: An optional profile image for the learner, resized to 300px wide and 70% quality.
+       - created_date: The date and time when the profile was created, set automatically.
+       - updated_date: The date and time when the profile was last updated, set automatically.
+
+       Methods:
+       - save: Overrides the save method to automatically generate a profile_id if it is not already set.
+
+       Meta:
+       - ordering: Orders the learner profiles by creation date in descending order.
+       """
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
     full_name = models.CharField(max_length=200, null=False, blank=False)
     bio = models.TextField(null=True, blank=True)
+    profile_id = models.CharField(max_length=255,unique=True, editable=False, default=uuid.uuid4)
     image = ResizedImageField(
         size=[300, None],
         quality=70,
@@ -18,18 +36,13 @@ class Learner(models.Model):
     created_date = models.DateField(auto_now_add=True)
     updated_date = models.DateField(auto_now=True)
 
+    def save(self, *args, **kwargs):
+        if not self.profile_id:
+            self.profile_id = str(uuid.uuid4())
+        super().save(*args, **kwargs)
+
     class Meta:
         ordering = ['-created_date']
 
     def __str__(self):
         return f"{self.owner}'s learner profile"
-
-# def create_learner_profile(sender, instance, created, **kwargs):
-#     """Creates profile when a new user is created"""
-#     if created:
-#         Learner.objects.create(owner=instance)
-#
-#
-# # signal to listen for when a new user is saved
-# # and create a learner profile
-# post_save.connect(create_learner_profile, sender=User)
--- learner/serializers.py
+++ learner/serializers.py
@@ -5,8 +5,28 @@ from .models import Learner
 
 
 class LearnerSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Learner model.
+
+    Fields:
+    - id: The unique identifier for the learner.
+    - owner: The user who owns the learner profile.
+    - full_name: The full name of the learner.
+    - bio: A brief biography of the learner.
+    - image: The profile image of the learner.
+    - profile_id: A unique identifier for the profile, read-only.
+    - created_date: The date when the profile was created.
+    - updated_date: The date when the profile was last updated.
+    - enrolled_courses_count: The number of courses the learner is enrolled in.
+    - enrolled_courses: A list of courses the learner is enrolled in.
+
+    Methods:
+    - get_enrolled_courses: Retrieves and serializes the courses the learner is enrolled in.
+    - get_enrolled_courses_count: Retrieves the count of courses the learner is enrolled in.
+    """
     enrolled_courses = serializers.SerializerMethodField()
     enrolled_courses_count = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField()
 
     def get_enrolled_courses(self, obj):
         enrolled_courses = obj.owner.courses_enrolled.all()
@@ -17,7 +37,7 @@ class LearnerSerializer(serializers.ModelSerializer):
         enrolled_courses = obj.owner.courses_enrolled.count()
         return enrolled_courses
 
-
     class Meta:
         model = Learner
-        fields = ['id', 'owner', 'full_name', 'bio', 'image', 'created_date', 'updated_date','enrolled_courses_count','enrolled_courses']
+        fields = ['id', 'owner', 'full_name', 'bio', 'image','profile_id', 'created_date', 'updated_date', 'enrolled_courses_count',
+                  'enrolled_courses']
